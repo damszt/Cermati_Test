bab08269f6ab738801d0f8938b526e36ff8880de
Adam Mac <adammac@Adams-MacBook-Air.local>
2024-09-21 14:11:31 +0700

    edit files

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,9 @@
 /// <reference types="Cypress" />
-describe('Login Cermati', () => {
-  it('Login Succes Valid Data', () => {
+describe('Cermati Gabung', () => {
+  it('Positive Case Valid Data', () => {
     cy.visit('https://www.cermati.com/app/gabung')
-    cy.get('#mobilePhone').type("081212121212")
-    cy.get('#email').type("test@gmail.com")
-    cy.get('#firstName').type("Syaiful")
-    cy.get('#lastName').type("Adam")
-    cy.get('.btn-track').click()
-  })
-  it('Login Succes Valid Data', () => {
-    cy.visit('https://www.cermati.com/app/gabung')
-    cy.get('#mobilePhone').type("08000000")
-    cy.get('#email').type("test@gmail.com")
+    cy.get('#mobilePhone').type("085649990579")
+    cy.get('#email').type("adamizzaaa@gmail.com")
     cy.get('#firstName').type("Syaiful")
     cy.get('#lastName').type("Adam")
     cy.get('.btn-track').click()
